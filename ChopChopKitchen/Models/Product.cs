7789aacd4f6e437bf7c7c8811b804e1f681f16ec
AHMEDSAEEDGIT <a.seed5513@gmail.com>
2025-04-25 13:15:57 +0300

    chop chop first commit

---
+++ ChopChopKitchen/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ChopChopKitchen.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+        //navigation property
+        public Category? Category { get; set; } // a products belongs to a category
+        public ICollection<OrderItem>? OrderItems { get; set; } // a product can be in many order items
+        public ICollection<ProductIngredient>? ProductIngredients { get; set; } // a product can have many ingredients
+    }
+}
